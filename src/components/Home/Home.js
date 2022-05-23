14735fcf950b66c7b49c5e380235c55312e231bf
MdRejoanMolla <mollamdrejoan@gmail.com>
2022-05-23 18:03:07 +0600

    business done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import BusinessSection from './BusinessSection';
 
 const Home = () => {
       return (
             <div>
                   <Banner></Banner>
+                  <BusinessSection></BusinessSection>
             </div>
       );
 };
