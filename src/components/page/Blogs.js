44f45b59c2a811f2856b7e2bc4b7a826fc4b1868
MdRejoanMolla <mollamdrejoan@gmail.com>
2022-05-23 16:52:36 +0600

    blog done

--- src/components/page/Blogs.js
+++ src/components/page/Blogs.js
@@ -2,31 +2,34 @@ import React from 'react';
 
 const Blogs = () => {
       return (
-            <div className="">
-                  <div className="">
-                        <p>14.1-</p>
+            <div className="border-8 text-xl rounded-lg border-teal-100 border-double m-1.5 p-2">
+                  <div className=" mb-4">
+                        <p><span className='text-neutral font-bold'>14.1 ans-</span> Learn how to use built-in APIs and third-party libraries to improve the performance of. your React applications. Find out more today at Web dEV! Website Performance. Website Audits. Structured Learning Paths.Keeping component state local where necessary.Code-splitting in React using dynamic import().Memoizing React components to prevent unnecessary re-renders.Windowing or list virtualization in React.</p>
+                        <div className="divider "></div>
                   </div>
-                  <div className="">
+                  <div className=" mb-4">
+                        <p><span className='text-neutral font-bold'>14.2 ans-</span> The state is an object that holds information about a certain component.Plain JavaScript functions don't have the ability to store information. The code within them executes and dissapears once the execution is finished.There are four main types of state you need to properly manage in your React apps:Local state,Global state,Server state,URL state</p>
+                        <div className="divider "></div>
+                  </div>
+                  <div className="mb-4">
+                        <p><span className='text-neutral font-bold'>14.3 ans-</span>The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object. getPrototypeOf and Object.prototypal inheritance supports the cloning of any object using an object linking mechanism.Prototypical inheritance allows us to reuse the properties or methods from one JavaScript object to another through a reference pointer function.</p><div className="divider "></div>
 
                   </div>
-                  <div className="">
+                  <div className="mb-4">
+                        <p><span className='text-neutral font-bold'>14.4 ans-</span>Before updating the value of the state, we need to build an initial state setup. Once we are done with it, we use the setState() method to change the state object. It ensures that the component has been updated and calls for re-rendering of the component.Mutating state directly can lead to odd bugs, and components that are hard to optimize.</p>
+                        <div className="divider "></div>
+                  </div>
+                  <div className="mb-4">
+                        <p><span className='text-neutral font-bold'>14.5 ans-</span>Yes .input field use</p>
+                        <div className="divider "></div>
+                  </div>
+                  <div className="mb-4">
+                        <p><span className='text-neutral font-bold'>14.6 ans-</span>UNIT TESTING is a type of software testing where individual units or components of a software are tested.Unit tests are typically automated tests written and run by software developers to ensure that a section of an application  meets its design and behaves as intended. In procedural programming, a unit could be an entire module, but it is more commonly an individual function or procedure.Arrange: set up the environment and prepare a bunch of objects to run the unit under test.- Act: call the unit under test.- Assert: check that outputs and side effects of the unit under test are as expected.</p>
+
+
 
                   </div>
-                  <div className="">
 
-                  </div>
-                  <div className="">
-
-                  </div>
-                  <div className="">
-
-                  </div>
-                  <div className="">
-
-                  </div>
-                  <div className="">
-
-                  </div>
             </div>
       );
 };
