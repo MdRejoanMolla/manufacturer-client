e3c9c1b4b52386f5b818c9cca3e3fb98909ddf28
MdRejoanMolla <mollamdrejoan@gmail.com>
2022-05-23 18:24:51 +0600

    about done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home/Home';
 import Footer from './components/Share/Footer';
 import Blogs from './components/page/Blogs.js';
 import NotFound from './components/page/NotFound';
+import About from './components/page/About';
 
 function App() {
       return (
@@ -14,6 +15,7 @@ function App() {
                   <Routes>
                         <Route path="/" element={<Home />}></Route>
                         <Route path="blogs" element={<Blogs />} />
+                        <Route path="about" element={<About />} />
                         <Route path="*" element={<NotFound />} />
                   </Routes>
                   <Footer></Footer>
---
+++ src/components/page/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+      return (
+            <div className='justify-center'>
+                  <img className='m-auto' src="https://i.ibb.co/k34DQtv/istockphoto-1163587800-1024x1024-1.jpg" alt="" />
+                  <p className='text-xl font-bold'>It seems everything was simpler in 2022 and bicycles were no exception. A common day of repairs consisted of a few flat tires, a broken spoke and possibly a coaster brake hub overhaul. As the 1960s arrived, so did caliper brakes, shifting systems, and lightweight components. One problem surfaced along with these complicated new parts. No tools! My father, Howard, and his business partner, Art Engstrom, were both "hands on" guys, usually figuring problems out for themselves. Both were up to the challenge as they crafted their own problem solvers. Most of these early tools were built from scratch with whatever materials and leftovers they could find.
+
+                        One of the first time-savers they invented was a stand that held the bike off the ground allowing easy, back saving access to any part of the bike. This original stand was put together using legs from a dining room table, an empty shell casing and a truck axle from a 37 Ford. As other shops, and eventually Schwinn Bicycle Company, saw the stand in action, requests were made for a production model. Park Tool Company was born!</p>
+            </div>
+      );
+};
+
+export default About;
